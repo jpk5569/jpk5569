dd6a3769eea8628c6ccb2ee1341d813f9eb7d8f9
jibankc <jpk5569@psu.edu>
2020-03-29 18:32:13 -0400

    menu added

--- src/edu/psu/abington/ist/ist242/Main.java
+++ src/edu/psu/abington/ist/ist242/Main.java
@@ -3,6 +3,4 @@ package edu.psu.abington.ist.ist242;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }dfddf
 }
---
+++ src/edu/psu/abington/ist/ist242/Menu.java
@@ -0,0 +1,30 @@
+package edu.psu.abington.ist.ist242;
+
+import java.util.ArrayList;
+
+    public class Menu {
+
+        //Class Level Variables - Protect the data
+        private int menuId;
+        private String menuItem;
+
+        //Constructor Method
+        public Menu(int _menuId, String _menuItem){
+            this.menuId = _menuId;
+            this.menuItem = _menuItem;
+        }
+
+        //Setters and Getters
+        public int getmenuId() { return menuId; }
+        public void setmenuId(int _menuId) {this.menuId = _menuId;}
+
+        public String getmenuItem() { return menuItem; }
+        public void setmenuItem(String _menuItem) {this.menuItem = _menuItem;}
+
+        public static void listMenu(ArrayList<Menu> mList){
+            for (Menu menu: mList){
+                System.out.println(menu.getmenuItem());
+            }
+        }
+    }
+
